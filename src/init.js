4523e14f01431bbd9899b0844f51c0d3d0a3f02c
Hilman Fikry <hilman12fikry@gmail.com>
2025-06-19 12:17:15 +0700

    refactor: improve localStorage code in init

--- src/init.js
+++ src/init.js
@@ -1,21 +1,20 @@
 import { Project, Task, projectList } from "./main-logic";
 
 function init() {
-    let projects;
-    window.onbeforeunload = function() {
-        localStorage.setItem('projects', JSON.stringify(projectList));
-    }
-    projects = JSON.parse(localStorage.getItem('projects'));
-    
-    if(projects) {
-        projects.forEach(project => {
-            project.taskList.forEach(task => Object.setPrototypeOf(task, Task.prototype));
-            Object.setPrototypeOf(project, Project.prototype);
-            projectList.push(project);
-        });
-    } else {
-        (new Project("General")).addProject();
-    }   
+  let projects;
+  window.onbeforeunload = function () {
+    localStorage.setItem("projects", JSON.stringify(projectList));
+  };
+  projects = JSON.parse(localStorage.getItem("projects"));
+
+  if (projects) {
+    projects.forEach((project) => {
+      const copiedProject = Project.fromJSON(project);
+      projectList.push(copiedProject);
+    });
+  } else {
+    new Project("General").addProject();
+  }
 }
 
 export default init;
--- src/main-logic.js
+++ src/main-logic.js
@@ -1,89 +1,96 @@
 import { format, isToday } from "date-fns";
 
 class Project {
-    constructor(name) {
-        this.name = name;
-        this.id = name.toLowerCase().replace(/ /g, "-");
-        this.taskList = [];
-    }
-    addProject() {
-        projectList.push(this);
-    }
-    deleteProject() {
-        projectList.splice(projectList.indexOf(this), 1);
-    }
-    renameProject(newName) {
-        this.name = newName;
-        this.id = newName.toLowerCase().replace(/ /g, "-");
-    }
-    addTask(task) {
-        this.taskList.push(task);
-    }
-    deleteTask(task) {
-        this.taskList.splice(this.taskList.indexOf(task), 1);
-    }
+  constructor(name) {
+    this.name = name;
+    this.id = name.toLowerCase().replace(/ /g, "-");
+    this.taskList = [];
+  }
+  addProject() {
+    projectList.push(this);
+  }
+  deleteProject() {
+    projectList.splice(projectList.indexOf(this), 1);
+  }
+  renameProject(newName) {
+    this.name = newName;
+    this.id = newName.toLowerCase().replace(/ /g, "-");
+  }
+  addTask(task) {
+    this.taskList.push(task);
+  }
+  deleteTask(task) {
+    this.taskList.splice(this.taskList.indexOf(task), 1);
+  }
+
+  static fromJSON(project) {
+    const newProject = new Project(project.name);
+    Object.assign(newProject, project);
+    newProject.taskList = project.taskList.map((task) => Task.fromJSON(task));
+    return newProject;
+  }
 }
 
 class Task {
-    constructor(name) {
-        this.name = name;
-        this.id = name.toLowerCase().replace(/ /g, "-");
-        this.priority = false;
-        this.dueDate = null;
-        this.note = '';
-        this.completion = false;
-    }
-    renameTask(newName) {
-        this.name = newName;
-        this.id = newName.toLowerCase().replace(/ /g, "-");
-    }
-    changePriority() {
-        this.priority = !this.priority;
-    }
-    changeDueDate(date) {
-        this.dueDate = date;
-    }
-    writeNote(text) {
-        this.note = text;
-    }
-    changeCompletion() {
-        this.completion = !this.completion;
-    }
+  constructor(name) {
+    this.name = name;
+    this.id = name.toLowerCase().replace(/ /g, "-");
+    this.priority = false;
+    this.dueDate = null;
+    this.note = "";
+    this.completion = false;
+  }
+  renameTask(newName) {
+    this.name = newName;
+    this.id = newName.toLowerCase().replace(/ /g, "-");
+  }
+  changePriority() {
+    this.priority = !this.priority;
+  }
+  changeDueDate(date) {
+    this.dueDate = date;
+  }
+  writeNote(text) {
+    this.note = text;
+  }
+  changeCompletion() {
+    this.completion = !this.completion;
+  }
+  static fromJSON(task) {
+    const newTask = new Task(task.name);
+    Object.assign(newTask, task);
+    return newTask;
+  }
 }
 
 class Main {
-    constructor(name, externalFn) {
-        this.name = name;
-        this.id = name.toLowerCase().replace(/ /g, "-");
-        this.externalFn = externalFn;
-        this.taskList = [];
-    }
-    updateList() {
-        this.taskList.length = 0;
-        projectList.forEach(project => {
-            project.taskList.forEach(task => {
-                if(this.externalFn(task)) {
-                    this.taskList.push(task);
-                }
-            });
-        });
-    }
+  constructor(name, externalFn) {
+    this.name = name;
+    this.id = name.toLowerCase().replace(/ /g, "-");
+    this.externalFn = externalFn;
+    this.taskList = [];
+  }
+  updateList() {
+    this.taskList.length = 0;
+    projectList.forEach((project) => {
+      project.taskList.forEach((task) => {
+        if (this.externalFn(task)) {
+          this.taskList.push(task);
+        }
+      });
+    });
+  }
 }
 
-const todayList = new Main('My Day', task => isToday(task.dueDate));
-const importantList = new Main('Important', task => task.priority);
-const plannedList = new Main('Planned', task => task.dueDate);
+const todayList = new Main("My Day", (task) => isToday(task.dueDate));
+const importantList = new Main("Important", (task) => task.priority);
+const plannedList = new Main("Planned", (task) => task.dueDate);
 
-todayList.assignDate = function() {
-    return format(new Date(), "iiii, MMMM d");
+todayList.assignDate = function () {
+  return format(new Date(), "iiii, MMMM d");
 };
 
 const projectList = [];
 const mainList = [todayList, importantList, plannedList];
 
-export {
-    Project,
-    Task,
-    projectList,
-    mainList
-};
+export { Project, Task, projectList, mainList };
